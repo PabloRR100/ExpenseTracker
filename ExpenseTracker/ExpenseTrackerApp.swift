bb8794e1bd67730dba86e3d87ab32477bc36a455
Pablo Ruiz <p_ruizruiz@apple.com>
2024-11-08 13:13:53 +0100

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Pablo Ruiz on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
